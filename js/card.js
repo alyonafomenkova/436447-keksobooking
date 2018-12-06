d8051d457cb2d226002479a95bed343df3e102bb
Alyona Fomenkova <fruitella30sep@mail.ru>
2018-12-07 00:34:11 +0300

    Broke the code into modules.

---
+++ js/card.js
@@ -0,0 +1,101 @@
+'use strict';
+
+(function () {
+  var CURRENCY_RUB = '\u20bd';
+  var PHOTOS_WIDTH = 45;
+  var PHOTOS_HEIGHT = 40;
+  var ESC_KEYCODE = 27;
+  var cardTemplate = document.querySelector('#card').content.querySelector('.map__card');
+  var mapFiltersContainer = document.querySelector('.map__filters-container');
+
+  function mapApartmentName(type) {
+    switch (type) {
+      case 'flat':
+        return 'Квартира';
+      case 'bungalo':
+        return 'Бунгало';
+      case 'house':
+        return 'Дом';
+      case 'palace':
+        return 'Дворец';
+      default:
+        return '';
+    }
+  }
+
+  function createFeatures(features) {
+    var fragment = document.createDocumentFragment();
+
+    for (var i = 0; i < features.length; i++) {
+      var featureItem = document.createElement('li');
+      featureItem.className = 'popup__feature' + ' popup__feature--' + features[i];
+      fragment.appendChild(featureItem);
+    }
+    return fragment;
+  }
+
+  function createPhotos(photoUrls) {
+    var fragment = document.createDocumentFragment();
+
+    for (var i = 0; i < photoUrls.length; i++) {
+      var img = document.createElement('img');
+      img.src = photoUrls[i];
+      img.classList.add('popup__photo');
+      img.width = PHOTOS_WIDTH;
+      img.height = PHOTOS_HEIGHT;
+      img.alt = 'Фотография жилья';
+      fragment.appendChild(img);
+    }
+    return fragment;
+  }
+
+  function createCardForApartment(apartment) {
+    var fragment = document.createDocumentFragment();
+    var cardElement = cardTemplate.cloneNode(true);
+    var features = apartment.offer.features;
+    var photos = apartment.offer.photos;
+
+    cardElement.querySelector('.popup__title').textContent = apartment.offer.title;
+    cardElement.querySelector('.popup__text--address').textContent = apartment.offer.address;
+    cardElement.querySelector('.popup__text--price').textContent = apartment.offer.price + CURRENCY_RUB + '/ночь';
+    cardElement.querySelector('.popup__type').textContent = mapApartmentName(apartment.offer.type);
+    cardElement.querySelector('.popup__text--capacity').textContent = apartment.offer.rooms + ' комнаты для ' + apartment.offer.guests + ' гостей';
+    cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + apartment.offer.checkin + ', выезд до ' + apartment.offer.checkout;
+    cardElement.replaceChild(createFeatures(features), cardElement.querySelector('.popup__features'));
+    cardElement.querySelector('.popup__description').textContent = apartment.offer.description;
+    cardElement.replaceChild(createPhotos(photos), cardElement.querySelector('.popup__photos'));
+    cardElement.querySelector('.popup__avatar').src = apartment.author.avatar;
+    cardElement.querySelector('.popup__close').addEventListener('click', onCardCloseClick);
+    document.addEventListener('keydown', onCardCloseEcsPress);
+
+    fragment.appendChild(cardElement);
+    return fragment;
+  }
+
+  function onCardCloseClick() {
+    destroyCard();
+    document.removeEventListener('keydown', onCardCloseEcsPress);
+  }
+
+  function onCardCloseEcsPress(evt) {
+    if (evt.keyCode === ESC_KEYCODE) {
+      destroyCard();
+      document.removeEventListener('keydown', onCardCloseEcsPress);
+    }
+  }
+
+  function destroyCard() {
+    var card = window.map.map.querySelector('.map__card');
+    window.map.map.removeChild(card);
+  }
+
+  window.card = {
+    showCard: function (apartment) {
+      var card = window.map.map.querySelector('.map__card');
+      if (card) {
+        destroyCard();
+      }
+      window.map.map.insertBefore(createCardForApartment(apartment), mapFiltersContainer);
+    }
+  }
+})();
---
+++ js/pin.js
@@ -0,0 +1,98 @@
+'use strict';
+
+(function () {
+
+  window.pin = {
+    STEM_OF_PIN_WIDTH: 10,
+    STEM_OF_PIN_HEIGHT: 32,
+    mapPinMain: document.querySelector('.map__pin--main'),
+
+    renderPinsForApartments: function (apartments) {
+      var fragment = document.createDocumentFragment();
+
+      for (var i = 0; i < apartments.length; i++) {
+        var apartment = apartments[i];
+        var pinElement = createPin(apartment);
+        pinElement.addEventListener('click', onPinClickListener.bind(null, apartment));
+        fragment.appendChild(pinElement);
+      }
+      return fragment;
+    }
+  }
+
+  var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+  var startCoords = {
+    x: 0,
+    y: 0
+  };
+
+  function createPin(apartment) {
+    var pinElement = pinTemplate.cloneNode(true);
+    pinElement.style = 'left: ' + (apartment.location.x - Math.round(window.pin.STEM_OF_PIN_WIDTH / 2)) + 'px; top: ' + (apartment.location.y - window.pin.STEM_OF_PIN_HEIGHT) + 'px;';
+    pinElement.querySelector('img').src = apartment.author.avatar;
+    pinElement.querySelector('img').alt = apartment.offer.description;
+    return pinElement;
+  }
+
+  function onPinClickListener(apartment) {
+    window.card.showCard(apartment);
+  }
+
+  function onMouseDown(downEvt) {
+    downEvt.preventDefault();
+    startCoords.x = downEvt.clientX;
+    startCoords.y = downEvt.clientY;
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  }
+
+  function onMouseMove(moveEvt) {
+    moveEvt.preventDefault();
+
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    var top = window.pin.mapPinMain.offsetTop - shift.y;
+    var left = window.pin.mapPinMain.offsetLeft - shift.x;
+
+    if (left < (window.map.LOCATION_X_MIN - window.pin.mapPinMain.offsetWidth / 2)) {
+      left = window.map.LOCATION_X_MIN - window.pin.mapPinMain.offsetWidth / 2;
+    }
+
+    if (left > (window.map.LOCATION_X_MAX - window.pin.mapPinMain.offsetWidth / 2)) {
+      left = window.map.LOCATION_X_MAX - window.pin.mapPinMain.offsetWidth / 2;
+    }
+
+    if (top < window.map.LOCATION_Y_MIN - window.pin.mapPinMain.offsetHeight) {
+      top = window.map.LOCATION_Y_MIN - window.pin.mapPinMain.offsetHeight;
+    }
+
+    if (top > window.map.LOCATION_Y_MAX - window.pin.mapPinMain.offsetHeight) {
+      top = window.map.LOCATION_Y_MAX - window.pin.mapPinMain.offsetHeight;
+    }
+    window.pin.mapPinMain.style.top = top + 'px';
+    window.pin.mapPinMain.style.left = left + 'px';
+  }
+
+  function onMouseUp(upEvt) {
+    upEvt.preventDefault();
+
+    if (!window.main.isPageActive) {
+      window.main.activateMapAndForms();
+    }
+
+    window.form.updateAddress();
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  }
+
+  window.pin.mapPinMain.addEventListener('mousedown', onMouseDown);
+})();
